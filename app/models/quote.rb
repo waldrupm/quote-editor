00a2f9019a89ae61ebe3642089371c05f4bb3628
Micheal Waldrup <waldrupm@gmail.com>
2022-09-21 15:20:04 -0400

    secure turbo streams and scope by company

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -18,5 +18,6 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # Those three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+  # [quote.company, "quotes"] allows the lambda to return unique values for the channel names, scoped to company, preventing broadcasting to other companies
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,7 +14,7 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name" do
+    assert_equal "Accountant", users(:accountant).name
+  end
 end
